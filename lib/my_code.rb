c342b7e1178111a4eb8ead1bb14d6a2a94e98b6a
Donny L <donny.landis@gmail.com>
2020-02-20 05:23:54 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -52,7 +52,7 @@ def reduce_to_all_true(array)
   new_array = []
   i = 0
   while i < array.length do
-    new_array << (array[i] ? array[i] : !array[i])
+    new_array << (array[i] ? array[i] : !!array[i])
     i += 1
   end
   new_array
